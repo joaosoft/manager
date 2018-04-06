175d733a08335c2f88504434fa0f5f0106e79070
João Ribeiro <joaosoft@gmail.com>
2018-04-06 18:49:33 +0100

    fix

--- service/global.go
+++ service/global.go
@@ -4,12 +4,13 @@ import (
 	"os"
 
 	logger "github.com/joaosoft/go-log/service"
+	writer "github.com/joaosoft/go-writer/service"
 )
 
 var global = make(map[string]interface{})
 var log = logger.NewLog(
 	logger.WithLevel(logger.InfoLevel),
-	logger.WithFormatHandler(logger.JsonFormatHandler),
+	logger.WithFormatHandler(writer.JsonFormatHandler),
 	logger.WithWriter(os.Stdout)).WithPrefixes(map[string]interface{}{
 	"level":   logger.LEVEL,
 	"time":    logger.TIME,
