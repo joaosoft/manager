b961424e4b944e6ea755454533b8dbf2a8ff0119
João Ribeiro <joaosoft@gmail.com>
2018-04-12 09:14:10 +0100

    doc

--- service/doc.go
+++ service/doc.go
@@ -17,6 +17,27 @@ at https://github.com/joaosoft/go-manager/tree/master/bin/launcher
 */
 package gomanager
 
+// You can place usage examples in your godoc.
+//
+// Examples should be placed in a file with a _test suffix. For example, the
+// examples in this guide are in a file called doc_test.go .
+//
+// The example functions should be called Example() for
+// package examples, ExampleTypename() for a specific type or
+// ExampleFuncname() for a specific function. For multiple examples
+// for the same entity (like same function), you can add a suffix like
+// ExampleFoo_suffix1, ExampleFoo_suffix2.
+//
+// You can document an example's output, by adding an output comment at its end.
+// The output comment must begin with "Output:", as shown below:
+//  func ExampleExamples_output() {
+//      fmt.Println("Hello")
+//      // Output: Hello
+//  }
+//
+// Notice that the tricks brought here (titles, code blocks, links etc.) don't work
+// in example documentation.
+//
 // For full documentation of examples, see:
 // http://golang.org/pkg/testing/
 type Examples int
