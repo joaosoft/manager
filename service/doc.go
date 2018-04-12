d735151c02d4111f8cc1150cd43aa76fa6f45925
João Ribeiro <joaosoft@gmail.com>
2018-04-12 09:21:07 +0100

    doc

--- service/doc.go
+++ service/doc.go
@@ -17,27 +17,54 @@ at https://github.com/joaosoft/go-manager/tree/master/bin/launcher
 */
 package gomanager
 
-// You can place usage examples in your godoc.
-//
-// Examples should be placed in a file with a _test suffix. For example, the
-// examples in this guide are in a file called doc_test.go .
-//
-// The example functions should be called Example() for
-// package examples, ExampleTypename() for a specific type or
-// ExampleFuncname() for a specific function. For multiple examples
-// for the same entity (like same function), you can add a suffix like
-// ExampleFoo_suffix1, ExampleFoo_suffix2.
-//
-// You can document an example's output, by adding an output comment at its end.
-// The output comment must begin with "Output:", as shown below:
-//  func ExampleExamples_output() {
-//      fmt.Println("Hello")
-//      // Output: Hello
-//  }
-//
-// Notice that the tricks brought here (titles, code blocks, links etc.) don't work
-// in example documentation.
-//
-// For full documentation of examples, see:
-// http://golang.org/pkg/testing/
-type Examples int
+// Please always use the parenthetical form of import
+// even though when only importing one package
+// import "fmt"
+// will work, it makes it uniform and simpler to add later
+
+import (
+	"math"
+)
+
+// You can godoc constants
+
+// Some enum examples
+const (
+	CONSTA = iota
+	CONSTB
+	CONSTC
+	CONSTD
+	ANOTHER = 7
+)
+
+// You can godoc vars
+
+// This is just a random variable
+var Default float64 = 0.7
+
+// var Default = float64(0.7) would've worked as well
+
+// You can godoc types
+
+// Example is a float used for demonstration purposes
+type Example float64
+
+// Example2 is also for demonstartion
+type Example2 struct {
+	X Example
+	y int // Private fields do not appear in godoc
+}
+
+// You can godoc functions
+
+// NewExample is used to get a ready-to-use Example2
+func NewExample(num int) *Example2 {
+	return &Example{0.0, num}
+}
+
+// You can godoc methods
+
+// Returns the square root of an example
+func (e Example) Sqrt() Example {
+	return Example(math.Sqrt(float64(e)))
+}
