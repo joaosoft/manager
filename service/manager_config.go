83645f931309d2ac178aa1a491930347b5f08a37
João Ribeiro <joaosoft@gmail.com>
2018-04-14 01:48:05 +0100

    fix config

--- service/manager_config.go
+++ service/manager_config.go
@@ -1,8 +1,23 @@
 package gomanager
 
+import "time"
+
 // IConfig ...
 type IConfig interface {
-	Get() interface{}
+	Get(key string) interface{}
+	GetString(key string) string
+	GetBool(key string) bool
+	GetInt(key string) int
+	GetInt64(key string) int64
+	GetFloat64(key string) float64
+	GetTime(key string) time.Time
+	GetDuration(key string) time.Duration
+	GetStringSlice(key string) []string
+	GetStringMap(key string) map[string]interface{}
+	GetStringMapString(key string) map[string]string
+	GetStringMapStringSlice(key string) map[string][]string
+
+	GetObj() interface{}
 	Set(config interface{})
 	Save() error
 	Reload() error
--- service/simple_config.go
+++ service/simple_config.go
@@ -1,9 +1,16 @@
 package gomanager
 
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
+
 type SimpleConfig struct {
 	file  string
 	obj   interface{}
 	bytes []byte
+	viper *viper.Viper
 }
 
 // NewSimpleConfig...
@@ -15,36 +22,108 @@ func NewSimpleConfig(file string, obj interface{}) (IConfig, error) {
 			file:  file,
 			obj:   obj,
 			bytes: bytes,
+			viper: loadViper(file),
 		}, err
 	}
 }
 
 // Get ...
-func (setup *SimpleConfig) Get() interface{} {
-	return setup.obj
+func (simple *SimpleConfig) Get(key string) interface{} {
+	return simple.viper.Get(key)
+}
+
+// GetString ...
+func (simple *SimpleConfig) GetString(key string) string {
+	return simple.viper.GetString(key)
+}
+
+// GetBool ...
+func (simple *SimpleConfig) GetBool(key string) bool {
+	return simple.viper.GetBool(key)
+}
+
+// GetInt ...
+func (simple *SimpleConfig) GetInt(key string) int {
+	return simple.viper.GetInt(key)
+}
+
+// GetInt64 ...
+func (simple *SimpleConfig) GetInt64(key string) int64 {
+	return simple.viper.GetInt64(key)
+}
+
+// GetFloat64 ...
+func (simple *SimpleConfig) GetFloat64(key string) float64 {
+	return simple.viper.GetFloat64(key)
+}
+
+// GetTime ...
+func (simple *SimpleConfig) GetTime(key string) time.Time {
+	return simple.viper.GetTime(key)
+}
+
+// GetDuration ...
+func (simple *SimpleConfig) GetDuration(key string) time.Duration {
+	return simple.viper.GetDuration(key)
+}
+
+// GetStringSlice ...
+func (simple *SimpleConfig) GetStringSlice(key string) []string {
+	return simple.viper.GetStringSlice(key)
+}
+
+// GetStringMap ...
+func (simple *SimpleConfig) GetStringMap(key string) map[string]interface{} {
+	return simple.viper.GetStringMap(key)
+}
+
+// GetStringMapString ...
+func (simple *SimpleConfig) GetStringMapString(key string) map[string]string {
+	return simple.viper.GetStringMapString(key)
+}
+
+// GetStringMapStringSlice ...
+func (simple *SimpleConfig) GetStringMapStringSlice(key string) map[string][]string {
+	return simple.viper.GetStringMapStringSlice(key)
+}
+
+// GetObj ...
+func (simple *SimpleConfig) GetObj() interface{} {
+	return simple.obj
 }
 
 // Set ...
-func (setup *SimpleConfig) Set(config interface{}) {
-	setup.obj = config
+func (simple *SimpleConfig) Set(config interface{}) {
+	simple.obj = simple
 }
 
 // Reload ...
-func (setup *SimpleConfig) Reload() error {
-	if bytes, err := readFile(setup.file, setup.obj); err != nil {
+func (simple *SimpleConfig) Reload() error {
+	if bytes, err := readFile(simple.file, simple.obj); err != nil {
 		return err
 	} else {
-		setup.bytes = bytes
+		simple.viper = loadViper(simple.file)
+		simple.bytes = bytes
 	}
 
 	return nil
 }
 
 // Save ...
-func (setup *SimpleConfig) Save() error {
-	if err := writeFile(setup.file, setup.obj); err != nil {
+func (simple *SimpleConfig) Save() error {
+	if err := writeFile(simple.file, simple.obj); err != nil {
 		return err
 	}
 
+	simple.viper = loadViper(simple.file)
+
 	return nil
+}
+
+func loadViper(file string) *viper.Viper {
+	viper := viper.New()
+	viper.AddConfigPath(file)
+	viper.ReadInConfig()
+
+	return viper
 }
