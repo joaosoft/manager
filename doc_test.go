e0a69783e8d31c3d9c3c43d2cf824cb5d1a6d710
João Ribeiro <joaosoft@gmail.com>
2019-01-20 20:51:33 +0000

    fix

--- doc_test.go
+++ doc_test.go
@@ -3,6 +3,7 @@ package manager
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/joaosoft/logger"
 	"math/rand"
 	"net/http"
 	"os"
@@ -16,7 +17,7 @@ import (
 
 // --------- dummy process ---------
 func dummy_process() error {
-	log.Info("hello, i'm exetuting the dummy process")
+	logger.Info("hello, i'm exetuting the dummy process")
 	return nil
 }
 
@@ -24,7 +25,7 @@ func dummy_process() error {
 type dummy_nsq_handler struct{}
 
 func (dummy *dummy_nsq_handler) HandleMessage(msg *nsq.Message) error {
-	log.Infof("executing the handle message of NSQ with [ message: %s ]", string(msg.Body))
+	logger.Infof("executing the handle message of NSQ with [ message: %s ]", string(msg.Body))
 	return nil
 }
 
@@ -53,7 +54,7 @@ func dummy_web_echo_handler(ctx echo.Context) error {
 }
 
 func work_handler(id string, data interface{}) error {
-	log.Infof("work with the id %s and data %s done!", id, data.(string))
+	logger.Infof("work with the id %s and data %s done!", id, data.(string))
 	return nil
 }
 
@@ -64,26 +65,26 @@ func usage() {
 
 	//
 	// Manager: processes
-	process := NewSimpleProcess(dummy_process)
+	process := manager.NewSimpleProcess(dummy_process)
 	if err := manager.AddProcess("process_1", process); err != nil {
-		log.Errorf("MAIN: error on processes %s", err)
+		logger.Errorf("MAIN: error on processes %s", err)
 	}
 
 	//
 	// nsq producer
 	nsqConfigProducer := NewNSQConfig("topic_1", "channel_1", []string{"127.0.0.1:4150"}, []string{"127.0.0.1:4161"}, 30, 5)
-	nsqProducer, _ := NewSimpleNSQProducer(nsqConfigProducer)
+	nsqProducer, _ := manager.NewSimpleNSQProducer(nsqConfigProducer)
 	manager.AddNSQProducer("nsq_producer_1", nsqProducer)
 	nsqProducer = manager.GetNSQProducer("nsq_producer_1")
 	nsqProducer.Publish("topic_1", []byte("MENSAGEM ENVIADA PARA A NSQ"), 3)
 
-	log.Info("waiting 1 seconds...")
+	logger.Info("waiting 1 seconds...")
 	<-time.After(time.Duration(1) * time.Second)
 
 	//
 	// Manager: nsq consumer
 	nsqConfigConsumer := NewNSQConfig("topic_1", "channel_1", []string{"127.0.0.1:4161"}, []string{"127.0.0.1:4150"}, 30, 5)
-	nsqConsumer, _ := NewSimpleNSQConsumer(nsqConfigConsumer, &dummy_nsq_handler{})
+	nsqConsumer, _ := manager.NewSimpleNSQConsumer(nsqConfigConsumer, &dummy_nsq_handler{})
 	manager.AddProcess("nsq_consumer_1", nsqConsumer)
 
 	//
@@ -103,80 +104,80 @@ func usage() {
 	config := manager.GetConfig("config_1")
 
 	jsonIndent, _ := json.MarshalIndent(config.GetObj(), "", "    ")
-	log.Infof("CONFIGURATION: %s", jsonIndent)
+	logger.Infof("CONFIGURATION: %s", jsonIndent)
 
 	// allows to set a new configuration and save in the file
 	n := rand.Intn(9000)
 	obj.User.Random = n
-	log.Infof("MAIN: Random: %d", n)
+	logger.Infof("MAIN: Random: %d", n)
 	config.Set(obj)
 	if err := config.Save(); err != nil {
-		log.Error("MAIN: error whe saving configuration file")
+		logger.Error("MAIN: error whe saving configuration file")
 	}
 
 	//
 	// Manager: web
 
 	// web - with http
-	web := NewSimpleWebHttp(":8081")
+	web := manager.NewSimpleWebHttp(":8081")
 	if err := manager.AddWeb("web_http", web); err != nil {
-		log.Error("error adding web process to Manager")
+		logger.Error("error adding web process to Manager")
 	}
 	web = manager.GetWeb("web_http")
 	web.AddRoute(http.MethodGet, "/web_http", dummy_web_http_handler)
 
 	// web - with echo
-	web = NewSimpleWebEcho(":8082")
+	web = manager.NewSimpleWebEcho(":8082")
 	if err := manager.AddWeb("web_echo", web); err != nil {
-		log.Error("error adding web process to Manager")
+		logger.Error("error adding web process to Manager")
 	}
 	web = manager.GetWeb("web_echo")
 	web.AddRoute(http.MethodGet, "/web_echo/:id", dummy_web_echo_handler)
 	go web.Start(&sync.WaitGroup{}) // starting this because of the gateway
 
-	log.Info("waiting 1 seconds...")
+	logger.Info("waiting 1 seconds...")
 	<-time.After(time.Duration(1) * time.Second)
 
 	//
 	// Manager: gateway
 	headers := map[string][]string{"Content-Type": {"application/json"}}
 
-	gateway := NewSimpleGateway()
+	gateway := manager.NewSimpleGateway()
 	manager.AddGateway("gateway_1", gateway)
 	gateway = manager.GetGateway("gateway_1")
 	status, bytes, err := gateway.Request(http.MethodGet, "http://127.0.0.1:8082", "/web_echo/123", headers, nil)
-	log.Infof("status: %d, response: %s, error? %t", status, string(bytes), err != nil)
+	logger.Infof("status: %d, response: %s, error? %t", status, string(bytes), err != nil)
 
 	//
 	// Manager: database
 
 	// database - postgres
 	postgresConfig := NewDBConfig("postgres", "postgres://user:password@localhost:7001?sslmode=disable")
-	postgresConn := NewSimpleDB(postgresConfig)
+	postgresConn := manager.NewSimpleDB(postgresConfig)
 	manager.AddDB("postgres", postgresConn)
 
 	// database - mysql
 	mysqlConfig := NewDBConfig("mysql", "root:password@tcp(127.0.0.1:7002)/mysql")
-	mysqlConn := NewSimpleDB(mysqlConfig)
+	mysqlConn := manager.NewSimpleDB(mysqlConfig)
 	manager.AddDB("mysql", mysqlConn)
 
 	//
 	// Manager: redis
 	redisConfig := NewRedisConfig("127.0.0.1", 7100, 0, "")
-	redisConn := NewSimpleRedis(redisConfig)
+	redisConn := manager.NewSimpleRedis(redisConfig)
 	manager.AddRedis("redis", redisConn)
 
 	//
 	// Manager: workqueue
 	workqueueConfig := NewWorkListConfig("queue_001", 1, 2, time.Second*2, FIFO)
-	workqueue := NewSimpleWorkList(workqueueConfig, work_handler)
+	workqueue := manager.NewSimpleWorkList(workqueueConfig, work_handler)
 	manager.AddWorkList("queue_001", workqueue)
 	workqueue = manager.GetWorkList("queue_001")
 	for i := 1; i <= 1000; i++ {
 		go workqueue.AddWork(fmt.Sprintf("PROCESS: %d", i), fmt.Sprintf("THIS IS MY MESSAGE %d", i))
 	}
 	if err := workqueue.Start(&sync.WaitGroup{}); err != nil {
-		log.Errorf("MAIN: error on workqueue %s", err)
+		logger.Errorf("MAIN: error on workqueue %s", err)
 	}
 
 	manager.Start()
